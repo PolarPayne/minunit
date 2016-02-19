028f891ec8bca326b8166d37f57e9968eb14c0d3
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-19 17:43:49 +0200

    Better asserts

--- minunit.h
+++ minunit.h
@@ -9,7 +9,39 @@
  * From http://www.jera.com/techinfo/jtns/jtn002.html
  */
 
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_assert(message, test) \
+	do { \
+		if (!(test)) \
+			return message "\n"; \
+	} while (0)
+
+#define mu_assert_fail(message)
+	mu_assert(message, 0)
+
+#define mu_assert_str_eq(message, tested, correct) \
+	mu_assert(message, strcmp(tested, correct) == 0)
+
+#define mu_assert_str_neq(message, tested, correct) \
+	mu_assert(message, strcmp(tested, correct) != 0)
+
+#define mu_assert_eq(message, tested, correct) \
+	mu_assert(message, tested == correct)
+
+#define mu_assert_neq(message, tested, correct) \
+	mu_assert(message, tested != correct)
+
+#define mu_assert_gt(message, tested, correct) \
+	mu_assert(message, tested > correct)
+
+#define mu_assert_ge(message, tested, correct) \
+	mu_assert(message, tested >= correct)
+
+#define mu_assert_lt(message, tested, correct) \
+	mu_assert(message, tested < correct)
+
+#define mu_assert_le(message, tested, correct) \
+	mu_assert(message, tested <= correct)
+
 #define mu_run_test(test) do { char *message = test(); mu_tests_run++; \
 			  if (message) return message; } while (0)
 extern int mu_tests_run;
